d8503f3edbaac6caaa0420ef0c56f76dc514003f
Jae Jin <jaej09@outlook.com>
2020-02-10 15:07:46 +0000

    Extracting Locations and Finishing up

--- indeed.py
+++ indeed.py
@@ -6,59 +6,72 @@ URL = f"https://au.indeed.com/jobs?q=(node+or+react)&limit={LIMIT}&radius=50"
 
 
 def extract_indeed_pages():
-  result = requests.get(URL)
+    result = requests.get(URL)
 
-  # BeautifulSoup
-  soup = BeautifulSoup(result.text, "html.parser")
-  # Find pagination
-  pagination = soup.find("div", {"class": "pagination"})
-  links = pagination.find_all("a")
-  pages = []
-  # Get all these paginations except the last one.
-  for link in links[0:-1]:
-    # Get only strings(texts) inside of link
-    pages.append(int(link.string))
-    # Get only the last value which is 20
-    max_pages = pages[-1]
+    # BeautifulSoup
+    soup = BeautifulSoup(result.text, "html.parser")
+    # Find pagination
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all("a")
+    pages = []
+    # Get all these paginations except the last one.
+    for link in links[0:-1]:
+        # Get only strings(texts) inside of link
+        pages.append(int(link.string))
+        # Get only the last value which is 20
+        max_pages = pages[-1]
 
-  return max_pages
+    return max_pages
 
 
 def extract_job(html):
-  # Get information from <a title="">
-  title = html.find("div", {"class": "title"}).a["title"]
-  company = html.find("span", {"class": "company"})
+    # Get information from <a title="">
+    title = html.find("div", {"class": "title"}).a["title"]
+    company = html.find("span", {"class": "company"})
 
-  # Sometimes company is None
-  if company == None:
-    company_anchor = "Unknown Company"
-  else:
-    company_anchor = company.find("a")
+    # Sometimes company is None
+    if company == None:
+        company_anchor = "Unknown Company"
+    else:
+        company_anchor = company.find("a")
 
-  # Sometimes <span class="company"> does not have a link <a>
-  if company_anchor == None: # there is no <a> tag
-    company = str(company.string)
-  elif company_anchor == "Unknown Company":
-    company = company_anchor
-  else: # there is a <a> tag
-    company = str(company_anchor.string)
+    # Sometimes <span class="company"> does not have a link <a>
+    if company_anchor == None:  # there is no <a> tag
+        company = str(company.string)
+    elif company_anchor == "Unknown Company":
+        company = company_anchor
+    else:  # there is a <a> tag
+        company = str(company_anchor.string)
 
-  # Remove spaces
-  company = company.strip()
+    # Remove spaces
+    company = company.strip()
 
-  return {"title": title, "company": company}
+    # <div class=location> can be hidden
+    # so we need to get the data from another tag.
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+
+    job_id = html["data-jk"]
+
+    return {
+        "title": title,
+        "company": company,
+        "location": location,
+        "link": f"https://au.indeed.com/viewjob?jk={job_id}"
+    }
 
 
 def extract_indeed_jobs(last_page):
-  jobs = []
-  for page in range(last_page):
-    result = requests.get(f"{URL}&start={page*LIMIT}")
-    # print(result.status_code)
-    soup = BeautifulSoup(result.text, "html.parser")
-    jobs = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-    for job in jobs:
-      if hasattr(job, 'find'):
-        job = extract_job(job)
-        jobs.append(job)
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        # print(result.status_code)
+        soup = BeautifulSoup(result.text, "html.parser")
+        jobs = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for job in jobs:
+            # Fix an error - 'dict' object has no attribute 'find' by adding hasattr(job, 'find')
+            if hasattr(job, 'find'):
+                job = extract_job(job)
+                jobs.append(job)
+            continue
 
-  return jobs
+    return jobs
