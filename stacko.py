67ea4f903af7050f62413049aeb3dbffe31abaeb
Jae Jin <jaej09@outlook.com>
2020-02-10 19:52:07 +0000

    StackOverflow extract_job

--- stacko.py
+++ stacko.py
@@ -13,13 +13,40 @@ def get_last_page():
     # Find pagination
     pagination = soup.find("div", {"class": "s-pagination"})
     pagination_links = pagination.find_all("a")
-    # Just want the last one 
+    # Just want the last one
     # NEXT 버튼 제외하면 last one 은 뒤에서 두번째에 위치함
     last_page = pagination_links[-2].get_text(strip=True)
-    return last_page
+    return int(last_page)
 
+
+def extract_job(html):
+    title = html.h2.a["title"]
+    # recursive=False -> h3 의 direct child <span> 만 가져오겠다고 명시
+    # variable 을 멀티로 주면 company = [0], location = [1] 들어감
+    company, location = html.h3.find_all("span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True)
+    
+    print(company, location)
+
+    return {"title": title, "company": company, "location": location}
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        extracted_info = soup.find_all("div", {"class": "-job"})
+        for info in extracted_info:
+            if info != None:
+                job = extract_job(info)
+                jobs.append(job)
+            continue
+        return jobs
 
 
 def get_jobs():
     last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return []
