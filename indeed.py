718e8f58c7d6985b8c404c092124be5ca8768242
Jae Jin <jaej09@outlook.com>
2020-02-10 14:17:46 +0000

    Fix an AttributeError - 'dict' object has no attribute 'find'

--- indeed.py
+++ indeed.py
@@ -24,23 +24,41 @@ def extract_indeed_pages():
   return max_pages
 
 
+def extract_job(html):
+  # Get information from <a title="">
+  title = html.find("div", {"class": "title"}).a["title"]
+  company = html.find("span", {"class": "company"})
+
+  # Sometimes company is None
+  if company == None:
+    company_anchor = "Unknown Company"
+  else:
+    company_anchor = company.find("a")
+
+  # Sometimes <span class="company"> does not have a link <a>
+  if company_anchor == None: # there is no <a> tag
+    company = str(company.string)
+  elif company_anchor == "Unknown Company":
+    company = company_anchor
+  else: # there is a <a> tag
+    company = str(company_anchor.string)
+
+  # Remove spaces
+  company = company.strip()
+
+  return {"title": title, "company": company}
+
+
 def extract_indeed_jobs(last_page):
+  jobs = []
   for page in range(last_page):
     result = requests.get(f"{URL}&start={page*LIMIT}")
     # print(result.status_code)
     soup = BeautifulSoup(result.text, "html.parser")
     jobs = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
     for job in jobs:
-      # Get information from <a title="">
-      title = job.find("div", {"class": "title"}).find("a")["title"]
-      company = job.find("span", {"class": "company"})
-      # Sometimes company does not have a link
-      company_anchor = company.find("a")
-      if company_anchor is not None:
-        company = str(company_anchor.string)
-      else:
-        company = str(company.string)
-      # Remove spaces
-      company = company.strip()
-      print(title, company)
+      if hasattr(job, 'find'):
+        job = extract_job(job)
+        jobs.append(job)
+
   return jobs
